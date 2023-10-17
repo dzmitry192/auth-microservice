5f474394aff011600744367e9e2b2d8ff4f50029
Dmitriy Sivachenko <dmitriysivachenko52@gmail.com>
2023-10-17 15:12:07 +0300

    Implementations of the UserDetails and UserDetailsService interfaces have been created. Also added findByEmail method to ClientRepository.

---
+++ src/main/java/com/innowise/authmicroservice/service/CustomUserDetails.java
@@ -0,0 +1,78 @@
+package com.innowise.authmicroservice.service;
+
+import com.innowise.authmicroservice.entity.ClientEntity;
+import com.innowise.authmicroservice.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private Role role;
+
+    public static CustomUserDetails build(ClientEntity client) {
+        return new CustomUserDetails(client.getId(),
+                client.getFirstName(),
+                client.getLastName(),
+                client.getEmail(),
+                client.getPassword(),
+                client.getPhoneNumber(),
+                client.getAddress(),
+                client.getRole());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/innowise/authmicroservice/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.innowise.authmicroservice.service;
+
+import com.innowise.authmicroservice.entity.ClientEntity;
+import com.innowise.authmicroservice.repository.ClientRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final ClientRepository clientRepository;
+
+    public CustomUserDetailsService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        ClientEntity client = clientRepository.findByEmail(email);
+        if(client == null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        return CustomUserDetails.build(client);
+    }
+}
